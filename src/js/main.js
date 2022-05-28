7b6abc20d2773be5a317ed041e309dd4ce1d4165
Dmitriy <tiemposilencioo@gmail.com>
2022-05-28 19:44:42 +0300

    createElement

--- src/js/main.js
+++ src/js/main.js
@@ -193,32 +193,47 @@ class MenuCard {
 
 }
 
-  new MenuCard(
-      "img/tabs/vegy.jpg",
-      "vegy",
-      'Меню "Фитнес"',
-      'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-      9,
-      ".menu .container",
-    ).render();
+const getResource = async (url) => {
+  const res = await fetch(url);
 
-  new MenuCard(
-      "img/tabs/post.jpg",
-      "post",
-      'Меню "Постное"',
-      'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-      14,
-      ".menu .container",
-    ).render();
+  if (!res.ok) {
+    throw new Error(`Could not fetch ${url}, status: ${res.status}`); 
+  }
 
-  new MenuCard(
-      "img/tabs/elite.jpg",
-      "elite",
-      'Меню “Премиум”',
-      'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-      21,
-      ".menu .container",
-    ).render();
+  return await res.json();
+};
+
+getResource('http://localhost:3000/menu')
+  .then(data => {
+    data.forEach(({img, altimg, title, descr, price}) => {
+      new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+    });
+  });
+
+// getResource('http://localhost:3000/menu')
+//   .then(data => createCard(data));
+
+//   function createCard (data) {
+//     data.forEach(({img, altimg, title, descr, price}) => {
+//       const element = document.createElement('div');
+
+//       element.classList.add('menu__item');
+
+//       element.innerHTML = `
+//       <img src=${img} alt=${altimg}>
+//       <h3 class="menu__item-subtitle">${title}</h3>
+//       <div class="menu__item-descr">${descr}</div>
+//       <div class="menu__item-divider"></div>
+//       <div class="menu__item-price">
+//           <div class="menu__item-cost">Цена:</div>
+//           <div class="menu__item-total"><span>${price}</span> грн/день</div>
+//       </div>
+//       `;
+
+//       document.querySelector('.menu .container').append(element);
+//     });
+//   }
+
 
     // Forms
 
@@ -231,10 +246,21 @@ class MenuCard {
           };
 
           forms.forEach(item => {
-            postData(item);
+            bindPostData(item);
           });
 
-    function postData(form) {
+    const postData = async (url, data) => {
+        const res = await fetch(url, {
+          method: 'POST',
+          headers: {
+            'Content-type': 'application/json; charset=utf-8'
+          },
+          body: data
+        });
+        return await res.json();
+    };
+
+    function bindPostData(form) {
       form.addEventListener('submit', (e) => {
         e.preventDefault();
 
@@ -247,19 +273,10 @@ class MenuCard {
         form.insertAdjacentElement('afterend', statusMessages);
         const formData = new FormData(form);
 
-        const object = {};
-        formData.forEach(function(value, key){
-            object[key] = value;
-        });
+        const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
-        fetch('server.php', {
-          method: 'POST',
-          headers: {
-            'Content-type': 'application/json; charset=utf-8'
-          },
-          body: JSON.stringify(object)
-        })
-        .then(data => data.text()) // ПРЕОБРАЗОВАТЬ В ТЕКСТ
+        postData('http://localhost:3000/requests', json)
+        // .then(data => data.text()) // ПРЕОБРАЗОВАТЬ В ТЕКСТ
         .then(data => {
             console.log(data);
             showThanksModal(messages.succes);
@@ -298,6 +315,9 @@ class MenuCard {
         }, 4000);
     }
 
+    // fetch('http://localhost:3000/menu')
+    //   .then(data => data.json())
+    //   .then(res => console.log(res));
       
     
 });
